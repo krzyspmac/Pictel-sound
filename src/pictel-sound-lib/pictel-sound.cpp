19ef3ae4115d65faeac28710813dbb7f57446390
Krzysztof Pawłowski <krzyspmac@gmail.com>
2022-05-28 10:43:36 +0200

    feat: initial commit

---
+++ src/pictel-sound-lib/pictel-sound.cpp
@@ -0,0 +1,10 @@
+//
+// Created by krzysp on 28/05/2022.
+//
+
+#include "include/pictel-sound.h"
+
+int MyPictelFunction()
+{
+    return 11;
+}
---
+++ src/pictel-sound-sample/macos/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "pictel-sound.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "My Pictel Function" << MyPictelFunction() << std::endl;
+    return 0;
+}
