b46c922129a48ef84e7572832becbbbc58961062
Krzysztof Pawłowski <krzyspmac@gmail.com>
2022-06-05 16:50:13 +0200

    feat: project cleanup

--- src/pictel-sound-lib/platform/windows/system_audio_manager.cpp
+++
@@ -1,9 +0,0 @@
-#include "system_audio_manager.h"
-
-#include <Windows.h>
-#include <wrl\implements.h>
-#include <mfapi.h>
-#include <AudioClient.h>
-#include <mmdeviceapi.h>
-
-using namespace PictelSound;
